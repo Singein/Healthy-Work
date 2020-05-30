f981891372ffb605b08e956817933e38ddb3e83c
singein <singein@outlook.com>
2020-05-30 11:25:48 +0800

    refactor(project): 完成结构化改造

--- setup.py
+++ setup.py
@@ -1,23 +1,3 @@
-import os
-from setuptools import find_packages, setup
 
-
-setup(
-    name='healthywork',
-    version='1.1.0',
-    packages=['healthywork'],
-    include_package_data=True,
-    license='MIT License',
-    description='生活不止有屏幕上的代码,还有诗和远方.',
-    url='https://github.com/Fuermohao/Healthy-Work',
-    author='Fuermohao',
-    author_email='Fuermohao@outlook.com',
-    platforms='Linux,Unix,Windows',
-    keywords='HealthyWork',
-    install_requires=['pyqt5'],
-    entry_points={
-        'console_scripts': [
-            'healthywork = healthywork.app:cli'
-        ]
-    }
-)
+import setuptools
+setuptools.setup(setup_requires=['pbr'], pbr=True)
